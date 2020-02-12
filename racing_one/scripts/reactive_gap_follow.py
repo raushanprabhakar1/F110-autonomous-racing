acd8f33123ff3e5bee57a6efa17937db3d678b62
wyw <yuweiwu20001@outlook.com>
2020-02-12 12:46:49 -0500

    update

--- racing_one/scripts/reactive_gap_follow.py
+++ racing_one/scripts/reactive_gap_follow.py
@@ -13,45 +13,52 @@ class reactive_follow_gap:
     def __init__(self):
         #Topics & Subscriptions,Publishers
         lidarscan_topic = '/scan'
-        #drive_topic = '/nav'
-        drive_topic =  '/vesc/high_level/ackermann_cmd_mux/input/nav_1'
+        drive_topic = '/nav'
+        #drive_topic =  '/vesc/high_level/ackermann_cmd_mux/input/nav_1'
 
         self.lidar_sub = rospy.Subscriber( lidarscan_topic, LaserScan, self.lidar_callback, queue_size=1)
         self.drive_pub = rospy.Publisher( drive_topic, AckermannDriveStamped, queue_size=1)
-    
+ 
+    @staticmethod
+    def running_mean(x,N):
+        cumsum = np.cumsum(np.insert(x,0,0))
+        return (cumsum[N:]- cumsum[:-N]) / float(N)
+
+   
     def preprocess_lidar(self, ranges):
         """ Preprocess the LiDAR scan array. Expert implementation includes:
             1.Setting each value to the mean over some window
             2.Rejecting high values (eg. > 3m)       """
         n = len(ranges)
-        proc_ranges = [0]*n
-        for i in range(n):
-            proc_ranges[i] = (ranges[i] + ranges[i-1] + ranges[i-2])/3
-            if ranges[i] < 1:
-                proc_ranges[i] = 0
-           
+        proc_ranges = self.running_mean(ranges, 4)
+        proc_ranges[proc_ranges < 1] = 0.0   
 
         return proc_ranges
 
     def find_max_gap(self, free_space_ranges):
         """ Return the start index & end index of the max gap in free_space_ranges
         """
-        start_i,end_i, best_start, best_end = 0,0,0,0
-        for i in range(len(free_space_ranges)):
-            if free_space_ranges[i] > 0:
-                end_i += 1
-            else:
-                if end_i != start_i and end_i - start_i + 1 > best_end-best_start+1:
-                    best_start = start_i
-                    best_end = end_i
-                
-                start_i = i
-                end_i = i
 
-        if end_i != start_i-1 and end_i - start_i + 1 > best_end-best_start+1:
-            best_start = start_i
-            best_end = end_i
-        return best_start, best_end
+        non_zeros= np.nonzero(free_space_ranges)
+        non_zeros = non_zeros[0]
+        #non_zeros = np.sort(non_zeros)  
+        non_zeros_2 = np.copy(non_zeros)    
+        non_zeros_2[:-1] = non_zeros[1:]
+        non_zeros_2[-1] = 0
+
+        result = non_zeros_2 - non_zeros
+
+        out = np.where(result>1)
+        # print(result)
+        if out[0].shape[0] > 0:
+            if out[0][0] +1 >= non_zeros.shape[0] - out[0][0] +1:
+                return 0, non_zeros[out[0][0]]
+            else:
+                return non_zeros[out[0][0]+1], free_space_ranges.shape[0]-1
+        else:
+            return 0, free_space_ranges.shape[0]-1
+
+
     
     def find_best_point(self, start_i, end_i, ranges):
 
@@ -65,7 +72,7 @@ class reactive_follow_gap:
         proc_ranges = self.preprocess_lidar(ranges)
 
         #Find closest point to LiDAR
-        index = proc_ranges.index(min(proc_ranges))
+        index = np.argmin(proc_ranges) #  proc_ranges.index(min(proc_ranges))
         min_distance = ranges[index]
 
         #Eliminate all points inside 'bubble' (set them to zero)
@@ -88,7 +95,7 @@ class reactive_follow_gap:
                 proc_ranges[i] = 0
 
 
-
+    
         #Find max length gap 
         start_i, end_i = self.find_max_gap(proc_ranges)
         #print([start_i, end_i])
@@ -114,19 +121,21 @@ class reactive_follow_gap:
         else:
             velocity = 2.5
 
-
+        
 
         angle = np.clip(angle, -0.43, 0.43)
 
-
+        #print(angle)
         #print(angle)
         drive_msg.header.stamp = rospy.Time.now()
         drive_msg.header.frame_id = "drive"
-        drive_msg.drive.speed = 1
+        drive_msg.drive.speed = 2
 
         drive_msg.drive.steering_angle = angle
         self.drive_pub.publish(drive_msg)
         return 
+
+   
 
 def main(args):
     rospy.init_node("FollowGap_node", anonymous=True)
